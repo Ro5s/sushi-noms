c1ce39a1dd9f652302e289c463e71ffc45c7d693
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-03-04 16:16:47 -0500

    Update Saave.sol
    bump to https://etherscan.io/address/0x364762c00b32c4b448f39efaa9cefc67a25603ff#code

--- contracts/stake/Saave.sol
+++ contracts/stake/Saave.sol
@@ -2,15 +2,11 @@
 
 pragma solidity 0.6.12;
 
-/// @dev brief interface for erc20 token.
-interface IERC20 { 
-    function balanceOf(address account) external view returns (uint256);
-    function approve(address spender, uint256 amount) external returns (bool);
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
-}
-
 /// @dev brief interface for entering SUSHI bar (xSUSHI).
 interface ISushiBarEnter { 
+    function balanceOf(address account) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
     function enter(uint256 amount) external;
 }
 
@@ -21,26 +17,26 @@ interface IAaveDeposit {
 
 /// @dev contract that batches SUSHI staking into AAVE xSUSHI (aXSUSHI).
 contract Saave {
-    address constant sushiToken = 0x6B3595068778DD592e39A122f4f5a5cF09C90fE2; // SUSHI token contract
-    address constant sushiBar = 0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272; // xSUSHI staking contract for SUSHI
-    address constant aave = 0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9; // AAVE lending pool contract for xSUSHI staking into aXSUSHI
+    ISushiBarEnter constant sushiToken = ISushiBarEnter(0x6B3595068778DD592e39A122f4f5a5cF09C90fE2); // SUSHI token contract
+    ISushiBarEnter constant sushiBar = ISushiBarEnter(0x8798249c2E607446EfB7Ad49eC89dD1865Ff4272); // xSUSHI staking contract for SUSHI
+    IAaveDeposit constant aave = IAaveDeposit(0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9); // AAVE lending pool contract for xSUSHI staking into aXSUSHI
     
     constructor() public {
-        IERC20(sushiToken).approve(sushiBar, type(uint256).max); // max approve `sushiBar` spender to stake SUSHI into xSUSHI from this contract
-        IERC20(sushiBar).approve(aave, type(uint256).max); // max approve `aave` spender to stake xSUSHI into aXSUSHI from this contract
+        sushiToken.approve(address(sushiBar), type(uint256).max); // max approve `sushiBar` spender to stake SUSHI into xSUSHI from this contract
+        sushiBar.approve(address(aave), type(uint256).max); // max approve `aave` spender to stake xSUSHI into aXSUSHI from this contract
     }
     
     /// @dev stake `amount` SUSHI into aXSUSHI by batching calls to `sushiBar` and `aave` lending pool.
     function saave(uint256 amount) external {
-        IERC20(sushiToken).transferFrom(msg.sender, address(this), amount); // deposit caller SUSHI `amount` into this contract
-        ISushiBarEnter(sushiBar).enter(amount); // stake deposited SUSHI `amount` into xSUSHI
-        IAaveDeposit(aave).deposit(sushiBar, IERC20(sushiBar).balanceOf(address(this)), msg.sender, 0); // stake resulting xSUSHI into aXSUSHI - send to caller
+        sushiToken.transferFrom(msg.sender, address(this), amount); // deposit caller SUSHI `amount` into this contract
+        sushiBar.enter(amount); // stake deposited SUSHI `amount` into xSUSHI
+        aave.deposit(address(sushiBar), sushiBar.balanceOf(address(this)), msg.sender, 0); // stake resulting xSUSHI into aXSUSHI - send to caller
     }
     
     /// @dev stake `amount` SUSHI into aXSUSHI for benefit of `to` by batching calls to `sushiBar` and `aave` lending pool.
     function saaveTo(address to, uint256 amount) external {
-        IERC20(sushiToken).transferFrom(msg.sender, address(this), amount); // deposit caller SUSHI `amount` into this contract
-        ISushiBarEnter(sushiBar).enter(amount); // stake deposited SUSHI `amount` into xSUSHI
-        IAaveDeposit(aave).deposit(sushiBar, IERC20(sushiBar).balanceOf(address(this)), to, 0); // stake resulting xSUSHI into aXSUSHI - send to `to`
+        sushiToken.transferFrom(msg.sender, address(this), amount); // deposit caller SUSHI `amount` into this contract
+        sushiBar.enter(amount); // stake deposited SUSHI `amount` into xSUSHI
+        aave.deposit(address(sushiBar), sushiBar.balanceOf(address(this)), to, 0); // stake resulting xSUSHI into aXSUSHI - send to `to`
     }
 }
