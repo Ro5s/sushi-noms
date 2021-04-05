19f6dd0993b19d635e1b474ede77aed3901fe572
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-05 14:01:45 -0400

    Create ISushiSwapLaunch.sol

---
+++ contracts/interfaces/ISushiSwapLaunch.sol
@@ -0,0 +1,26 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+interface ISushiSwapLaunch {
+    function createPair(address tokenA, address tokenB) external returns (address pair);
+    
+    function addLiquidity(
+        address tokenA,
+        address tokenB,
+        uint amountADesired,
+        uint amountBDesired,
+        uint amountAMin,
+        uint amountBMin,
+        address to,
+        uint deadline
+    ) external returns (uint amountA, uint amountB, uint liquidity);
+    
+    function addLiquidityETH(
+        address token,
+        uint amountTokenDesired,
+        uint amountTokenMin,
+        uint amountETHMin,
+        address to,
+        uint deadline
+    ) external payable returns (uint amountToken, uint amountETH, uint liquidity);
+}
