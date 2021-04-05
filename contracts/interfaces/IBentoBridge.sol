b2770af8619586469440bc8c3cb2e34131dea78c
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-05 14:03:37 -0400

    Create IBentoBridge.sol

---
+++ contracts/interfaces/IBentoBridge.sol
@@ -0,0 +1,25 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+/// @notice Interface for BENTO deposit and withdraw.
+interface IBentoBridge {
+    function balanceOf(IERC20, address) external view returns (uint256);
+
+    function registerProtocol() external;
+
+    function deposit( 
+        IERC20 token_,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external payable returns (uint256 amountOut, uint256 shareOut);
+
+    function withdraw(
+        IERC20 token_,
+        address from,
+        address to,
+        uint256 amount,
+        uint256 share
+    ) external returns (uint256 amountOut, uint256 shareOut);
+}}
