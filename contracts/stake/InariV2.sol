e6c9d467e51ff97279a15d0541705979779c0f16
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-09 20:41:47 -0400

    Create InariV2.sol

---
+++ contracts/stake/InariV2.sol
@@ -0,0 +1,42 @@
+/// SPDX-License-Identifier: MIT
+/*
+▄▄█    ▄   ██   █▄▄▄▄ ▄█ 
+██     █  █ █  █  ▄▀ ██ 
+██ ██   █ █▄▄█ █▀▀▌  ██ 
+▐█ █ █  █ █  █ █  █  ▐█ 
+ ▐ █  █ █    █   █    ▐ 
+   █   ██   █   ▀   
+           ▀          */
+/// Special thanks to Keno, Boring and Gonpachi for review and inspiration of early Inari patterns.
+pragma solidity 0.6.12;
+pragma experimental ABIEncoderV2;
+contract Inari {
+    uint public count;
+    mapping(uint => Offering) public offerings;
+    
+    event MakeOffering(address indexed to, bytes4 sig);
+    
+    struct Offering {
+        address to;
+        bytes4 sig;
+    }
+
+    function inari(
+        uint[] calldata offering, 
+        uint[] calldata value, 
+        bytes[] calldata param
+    ) external payable returns (bool success, bytes memory returnData) {
+        for (uint256 i = 0; i < offering.length; i++) {
+            bytes memory offer = abi.encode(offerings[offering[i]].sig, param[i]);
+            (success, returnData) = offerings[offering[i]].to.call{value: value[i]}(offer);
+            require(success, "!served");
+        }
+    }
+    
+    function makeOffering(address to, bytes4 sig) external {
+        offerings[count].to = to;
+        offerings[count].sig = sig;
+        count++;
+        emit MakeOffering(to, sig);
+    }
+}
