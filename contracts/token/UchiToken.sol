a1d21f0c0097cb126268bc439e8608f1e3690b9b
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-04-19 10:18:12 -0400

    Update UchiToken.sol
    typographic changes

--- contracts/token/UchiToken.sol
+++ contracts/token/UchiToken.sol
@@ -15,8 +15,8 @@ pragma solidity 0.8.3;
 
 /// @notice Interface for SushiSwap pair creation and ETH liquidity provision.
 interface ISushiSwapLaunch {
-    function approve(address spender, uint256 amount) external returns (bool); 
-    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+    function approve(address to, uint amount) external returns (bool); 
+    function transferFrom(address from, address to, uint amount) external returns (bool);
     function createPair(address tokenA, address tokenB) external returns (address pair);
     function addLiquidityETH(
         address token,
@@ -37,28 +37,28 @@ contract UchiToken {
     string public name;
     string public symbol;
     uint8 constant public decimals=18;
-    uint256 public totalSupply;
-    uint256 immutable public totalSupplyCap;
+    uint public totalSupply;
+    uint immutable public totalSupplyCap;
     bool public uchiRestricted;
     
-    mapping(address=>mapping(address=>uint256)) public allowance;
-    mapping(address=>uint256) public balanceOf;
+    mapping(address=>mapping(address=>uint)) public allowance;
+    mapping(address=>uint) public balanceOf;
     mapping(address=>bool) public uchi;
     
-    event Approval(address indexed owner, address indexed spender, uint256 amount);
-    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint amount);
+    event Transfer(address indexed from, address indexed to, uint amount);
     event UpdateUchi(address indexed account, bool approved);
     
     constructor(
         address[] memory _uchi, // initial whitelist array of accounts
         string memory _name, // erc20-formatted UchiToken 'name'
         string memory _symbol, // erc20-formatted UchiToken 'symbol'
-        uint256 _totalSupplyCap, // supply cap for UchiToken mint
-        uint256 pairDistro, // UchiToken amount minted for `sushiPair`
-        uint256[] memory uchiDistro, // UchiToken amount minted to `uchi`
+        uint _totalSupplyCap, // supply cap for UchiToken mint
+        uint pairDistro, // UchiToken amount minted for `sushiPair`
+        uint[] memory uchiDistro, // UchiToken amount minted to `uchi`
         bool market // if 'true', launch pair and add ETH liquidity on SushiSwap via 'Factory'
     ){
-        for(uint256 i=0;i<_uchi.length;i++){
+        for(uint i=0;i<_uchi.length;i++){
             balanceOf[_uchi[i]]=uchiDistro[i];
             totalSupply+=uchiDistro[i];
             uchi[_uchi[i]]=true;
@@ -71,32 +71,32 @@ contract UchiToken {
             balanceOf[msg.sender]=pairDistro;
             totalSupply+=pairDistro;
             emit Transfer(address(0), msg.sender, pairDistro);}
-        require(totalSupply<=_totalSupplyCap,"capped"); 
+        require(totalSupply<=_totalSupplyCap,'capped'); 
         governance=_uchi[0]; // first `uchi` is `governance`
         name=_name;
         symbol=_symbol;
         totalSupplyCap=_totalSupplyCap;
         uchiRestricted=true;
-        balanceOf[address(this)]=type(uint256).max; // max local balance blocks sends to UchiToken via overflow check (+saves gas)
+        balanceOf[address(this)]=type(uint).max; // max local balance blocks sends to UchiToken via overflow check (+saves gas)
     }
 
     /// - RESTRICTED ERC20 - ///
-    function approve(address to, uint256 amount) external returns (bool) {
+    function approve(address to, uint amount) external returns (bool) {
         allowance[msg.sender][to]=amount;
         emit Approval(msg.sender, to, amount);
         return true;
     }
     
-    function transfer(address to, uint256 amount) external returns (bool) {
-        if(uchiRestricted){require(uchi[msg.sender]&&uchi[to],"!uchi");}
+    function transfer(address to, uint amount) external returns (bool) {
+        if(uchiRestricted){require(uchi[msg.sender]&&uchi[to],'!uchi');}
         balanceOf[msg.sender]-=amount;
         balanceOf[to]+=amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
     
-    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
-        if(uchiRestricted){require(uchi[from]&&uchi[to],"!uchi");}
+    function transferFrom(address from, address to, uint amount) external returns (bool) {
+        if(uchiRestricted){require(uchi[from]&&uchi[to],'!uchi');}
         allowance[from][msg.sender]-=amount;
         balanceOf[from]-=amount;
         balanceOf[to]+=amount;
@@ -106,12 +106,12 @@ contract UchiToken {
     
     /// - GOVERNANCE - ///
     modifier onlyGovernance {
-        require(msg.sender==governance,"!governance");
+        require(msg.sender==governance,'!governance');
         _;
     }
     
-    function mint(address to, uint256 amount) external onlyGovernance {
-        require(totalSupply+amount<=totalSupplyCap,"capped"); 
+    function mint(address to, uint amount) external onlyGovernance {
+        require(totalSupply+amount<=totalSupplyCap,'capped'); 
         balanceOf[to]+=amount; 
         totalSupply+=amount; 
         emit Transfer(address(0), to, amount); 
@@ -122,7 +122,7 @@ contract UchiToken {
     }
 
     function updateUchi(address[] calldata account, bool[] calldata approved) external onlyGovernance {
-        for(uint256 i=0;i<account.length;i++){
+        for(uint i=0;i<account.length;i++){
             uchi[account[i]]=approved[i];
             emit UpdateUchi(account[i], approved[i]);
         }
@@ -138,19 +138,19 @@ contract UchiTokenFactory {
     ISushiSwapLaunch constant sushiSwapRouter=ISushiSwapLaunch(0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506);
     address public uchiDAO=msg.sender;
 
-    mapping(address=>uint256) public uchiList;
+    mapping(address=>uint) public uchiList;
     
     event DeployUchiToken(address indexed uchiToken);
-    event UpdateUchiList(address indexed account, uint256 indexed list, string details);
+    event UpdateUchiList(address indexed account, uint indexed list, string details);
     
     function deployUchiToken(
         address[] calldata _uchi, // initial whitelist array of accounts
         string calldata _name, // erc20-formatted UchiToken 'name'
         string calldata _symbol, // erc20-formatted UchiToken 'symbol'
-        uint256 _totalSupplyCap, // supply cap for UchiToken mint
-        uint256 pairDistro, // UchiToken amount minted for `sushiPair`
-        uint256[] calldata uchiDistro, // UchiToken amount minted to `uchi`
-        uint256 list, // if not '0', add check to `uchi` against given `uchiList`
+        uint _totalSupplyCap, // supply cap for UchiToken mint
+        uint pairDistro, // UchiToken amount minted for `sushiPair`
+        uint[] calldata uchiDistro, // UchiToken amount minted to `uchi`
+        uint list, // if not '0', add check to `uchi` against given `uchiList`
         bool market // if 'true', launch pair and add ETH liquidity on SushiSwap
     ) external payable returns (UchiToken uchiToken) {
         if(list!=0){checkList(_uchi, list);}
@@ -168,25 +168,25 @@ contract UchiTokenFactory {
         emit DeployUchiToken(address(uchiToken));
     }
     
-    function checkList(address[] calldata _uchi, uint256 list) private view { // deployment helper to avoid `stack too deep` error
-        for(uint256 i=0;i<_uchi.length;i++){require(uchiList[_uchi[i]]==list,"!listed");}
+    function checkList(address[] calldata _uchi, uint list) private view { // deployment helper to avoid `stack too deep` error
+        for(uint i=0;i<_uchi.length;i++){require(uchiList[_uchi[i]]==list,'!listed');}
     }
     
-    function initMarket(address uchiToken, uint256 pairDistro, address governance) private { // deployment helper to avoid `stack too deep` error
+    function initMarket(address uchiToken, uint pairDistro, address governance) private { // deployment helper to avoid `stack too deep` error
         sushiSwapRouter.addLiquidityETH{value: msg.value}(uchiToken, pairDistro, 0, 0, governance, 2533930386);
     }
     
     /// - GOVERNANCE - ///
-    function updateUchiList(address[] calldata account, uint256[] calldata list, string calldata details) external { // `0` is default and delisting action
-        require(msg.sender==uchiDAO,"!uchiDAO");
-        for(uint256 i=0;i<account.length;i++){
+    function transferGovernance(address _uchiDAO) external {
+        require(msg.sender==uchiDAO,'!uchiDAO');
+        uchiDAO=_uchiDAO;
+    }
+    
+    function updateUchiList(address[] calldata account, uint[] calldata list, string calldata details) external { // `0` is default and delisting action
+        require(msg.sender==uchiDAO,'!uchiDAO');
+        for(uint i=0;i<account.length;i++){
             uchiList[account[i]]=list[i]; 
             emit UpdateUchiList(account[i], list[i], details);
         }
-    }
-    
-    function transferGovernance(address _uchiDAO) external {
-        require(msg.sender==uchiDAO,"!uchiDAO");
-        uchiDAO=_uchiDAO;
     }
 }
