a45ac8c2d85bf105a7bca548b304d31d642595cb
Ross Campbell <41117279+Ro5s@users.noreply.github.com>
2021-05-13 15:37:55 -0400

    Update NyanSushi.sol
    simplify token code

--- contracts/stake/NyanSushi.sol
+++ contracts/stake/NyanSushi.sol
@@ -51,7 +51,7 @@ contract Domain {
 // File @boringcrypto/boring-solidity/contracts/ERC20.sol@v1.2.0
 // License-Identifier: MIT
 
-// Data part taken out for building of contracts that receive delegate calls
+/// @dev Data part taken out for building of contracts that receive delegate calls.
 contract ERC20Data {
     /// @notice owner > balance mapping.
     mapping(address => uint256) public balanceOf;
@@ -62,25 +62,16 @@ contract ERC20Data {
 }
 
 contract ERC20 is ERC20Data, Domain {
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 amount);
 
     /// @notice Transfers `amount` tokens from `msg.sender` to `to`.
     /// @param to The address to move the tokens.
     /// @param amount of the tokens to move.
     /// @return (bool) Returns True if succeeded.
-    function transfer(address to, uint256 amount) public returns (bool) {
-        // If `amount` is 0, or `msg.sender` is `to` nothing happens
-        if (amount != 0) {
-            uint256 srcBalance = balanceOf[msg.sender];
-            require(srcBalance >= amount, "ERC20: balance too low");
-            if (msg.sender != to) {
-                require(to != address(0), "ERC20: no zero address"); // Moved down so low balance calls save some gas
-
-                balanceOf[msg.sender] = srcBalance - amount; // Underflow is checked
-                balanceOf[to] += amount; // Can't overflow because totalSupply would be greater than 2^256-1
-            }
-        }
+    function transfer(address to, uint256 amount) external returns (bool) {
+        balanceOf[msg.sender] -= amount; 
+        balanceOf[to] += amount;
         emit Transfer(msg.sender, to, amount);
         return true;
     }
@@ -90,29 +81,14 @@ contract ERC20 is ERC20Data, Domain {
     /// @param to The address to move the tokens.
     /// @param amount The token amount to move.
     /// @return (bool) Returns True if succeeded.
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public returns (bool) {
-        // If `amount` is 0, or `from` is `to` nothing happens
-        if (amount != 0) {
-            uint256 srcBalance = balanceOf[from];
-            require(srcBalance >= amount, "ERC20: balance too low");
-
-            if (from != to) {
-                uint256 spenderAllowance = allowance[from][msg.sender];
-                // If allowance is infinite, don't decrease it to save on gas (breaks with EIP-20).
-                if (spenderAllowance != type(uint256).max) {
-                    require(spenderAllowance >= amount, "ERC20: allowance too low");
-                    allowance[from][msg.sender] = spenderAllowance - amount; // Underflow is checked
-                }
-                require(to != address(0), "ERC20: no zero address"); // Moved down so other failed calls safe some gas
-
-                balanceOf[from] = srcBalance - amount; // Underflow is checked
-                balanceOf[to] += amount; // Can't overflow because totalSupply would be greater than 2^256-1
-            }
+    function transferFrom(address from, address to, uint256 amount) external returns (bool) {
+        uint256 spenderAllowance = allowance[from][msg.sender];
+        // If allowance is infinite, don't decrease it to save on gas (breaks with EIP-20).
+        if (spenderAllowance != type(uint256).max) {
+            allowance[from][msg.sender] -= amount;
         }
+        balanceOf[from] -= amount;
+        balanceOf[to] += amount; 
         emit Transfer(from, to, amount);
         return true;
     }
@@ -131,28 +107,28 @@ contract ERC20 is ERC20Data, Domain {
     bytes32 private constant PERMIT_SIGNATURE_HASH = 0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
     /// @notice Approves `value` from `owner_` to be spend by `spender`.
-    /// @param owner_ Address of the owner.
+    /// @param owner Address of the owner.
     /// @param spender The address of the spender that gets approved to draw from `owner_`.
-    /// @param value The maximum collective amount that `spender` can draw.
+    /// @param amount The maximum collective amount that `spender` can draw.
     /// @param deadline This permit must be redeemed before this deadline (UTC timestamp in seconds).
     function permit(
-        address owner_,
+        address owner,
         address spender,
-        uint256 value,
+        uint256 amount,
         uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external {
-        require(owner_ != address(0), "ERC20: Owner cannot be 0");
+        require(owner != address(0), "ERC20: Owner cannot be 0");
         require(block.timestamp < deadline, "ERC20: Expired");
         require(
-            ecrecover(_getDigest(keccak256(abi.encode(PERMIT_SIGNATURE_HASH, owner_, spender, value, nonces[owner_]++, deadline))), v, r, s) ==
-                owner_,
+            ecrecover(_getDigest(keccak256(abi.encode(PERMIT_SIGNATURE_HASH, owner, spender, amount, nonces[owner]++, deadline))), v, r, s) ==
+                owner,
             "ERC20: Invalid Signature"
         );
-        allowance[owner_][spender] = value;
-        emit Approval(owner_, spender, value);
+        allowance[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
     }
 }
 
